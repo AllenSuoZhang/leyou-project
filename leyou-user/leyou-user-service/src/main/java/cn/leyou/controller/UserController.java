bc6c2a62b2c6e7b1031b8347ac3ddee81c996570
Lun-Yh <171319334@qq.com>
2019-08-12 22:18:18 +0800

    用户模块

---
+++ leyou-user/leyou-user-service/src/main/java/cn/leyou/controller/UserController.java
@@ -0,0 +1,13 @@
+package cn.leyou.controller;
+
+import cn.leyou.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    
+}
---
+++ leyou-user/leyou-user-service/src/main/java/cn/leyou/service/UserService.java
@@ -0,0 +1,12 @@
+package cn.leyou.service;
+
+import cn.leyou.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+}
