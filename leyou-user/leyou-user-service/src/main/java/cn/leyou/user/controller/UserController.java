381c68e316e8795915d84fae79162f1b9f12555d
AllenSuoZhang <171319334@qq.com>
2019-08-19 18:23:01 +0800

    短信，注册，登陆查询

--- leyou-user/leyou-user-service/src/main/java/cn/leyou/user/controller/UserController.java
+++ leyou-user/leyou-user-service/src/main/java/cn/leyou/user/controller/UserController.java
@@ -1,12 +1,14 @@
 package cn.leyou.user.controller;
 
+import cn.leyou.pojo.User;
 import cn.leyou.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -28,5 +30,55 @@ public class UserController {
         }
         return ResponseEntity.ok(boo);
     }
-    
+
+
+    /**
+     * 发送手机验证码
+     * @param phone
+     * @return
+     */
+    @PostMapping("code")
+    public ResponseEntity<Void> sendVerifyCode(@RequestParam("phone") String phone) {
+        Boolean boo = this.userService.sendVerifyCode(phone);
+        if (boo == null || !boo) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
+    /**
+     * 注册
+     * @param user
+     * @param code
+     * @return
+     */
+    @PostMapping("register")
+    public ResponseEntity<Void> register(@Valid User user, @RequestParam(value = "code", required = true)String code){
+        this.userService.register(user, code);
+        Boolean boo = this.userService.register(user, code);
+        if (boo == null || !boo) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
+    /**
+     * 根据用户名和密码查询用户
+     * @param username
+     * @param password
+     * @return
+     */
+    @GetMapping("query")
+    public ResponseEntity<User> queryUser(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password
+    ) {
+        User user = this.userService.queryUser(username, password);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(user);
+    }
 }
--- leyou-user/leyou-user-service/src/main/java/cn/leyou/user/service/UserService.java
+++ leyou-user/leyou-user-service/src/main/java/cn/leyou/user/service/UserService.java
@@ -1,9 +1,22 @@
 package cn.leyou.user.service;
 
+import cn.leyou.common.util.NumberUtils;
 import cn.leyou.user.mapper.UserMapper;
 import cn.leyou.pojo.User;
+import cn.leyou.user.utils.CodecUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserService {
@@ -11,6 +24,22 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    static final String KEY_PREFIX = "user:code:phone:";
+
+    static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    /**
+     * 检查数据
+     * @param data
+     * @param type
+     * @return
+     */
     public Boolean checkData(String data, Integer type) {
         User record = new User();
         switch (type) {
@@ -24,5 +53,79 @@ public class UserService {
                 return null;
         }
         return this.userMapper.selectCount(record) == 0;
+    }
+
+    /**
+     * 阿里云短信验证码
+     * @param phone
+     * @return
+     */
+    public Boolean sendVerifyCode(String phone) {
+        if (StringUtils.isEmpty(phone)){
+            return false;
+        }
+        // 生成验证码
+        String code = NumberUtils.generateCode(6);
+        try {
+            // 发送短信
+            Map<String, String> msg = new HashMap<>();
+            msg.put("phone", phone);
+            msg.put("code", code);
+            this.amqpTemplate.convertAndSend("leyou.sms.exchange", "sms.verify.code", msg);
+            // 将code存入redis
+            this.redisTemplate.opsForValue().set(KEY_PREFIX + phone, code, 90, TimeUnit.SECONDS);
+            return true;
+        } catch (Exception e) {
+            logger.error("发送短信失败。phone：{}， code：{}", phone, code);
+            return false;
+        }
+    }
+
+    /**
+     * 注册
+     * @param user
+     * @param code
+     */
+    @Transactional
+    public Boolean register(User user, String code) {
+        //从缓存中获取验证码
+        String cacheCode = this.redisTemplate.opsForValue().get(KEY_PREFIX + user.getPhone());
+        if (!org.apache.commons.lang3.StringUtils.equals(code, cacheCode)) {
+            return false;
+        }
+        //获取盐值
+        String salt = CodecUtils.generateSalt();
+        user.setSalt(salt);
+
+        //密码加盐加密
+        user.setPassword(CodecUtils.md5Hex(user.getPassword(), salt));
+
+        //新增用户
+        user.setId(null);
+        user.setCreated(new Date());
+        return this.userMapper.insertSelective(user) >= 1;
+    }
+
+    /**
+     * 登录查询
+     * @param username
+     * @param password
+     * @return
+     */
+    public User queryUser(String username, String password) {
+        // 查询
+        User record = new User();
+        record.setUsername(username);
+        User user = this.userMapper.selectOne(record);
+        // 校验用户名
+        if (user == null) {
+            return null;
+        }
+        // 校验密码
+        if (!user.getPassword().equals(CodecUtils.md5Hex(password, user.getSalt()))) {
+            return null;
+        }
+        // 用户名密码都正确
+        return user;
     }
 }
